a6f1fbb23fdd6bfca78a415e298fe80b66824020
Deibys Pinto <dmpintog801jt@gmail.com>
2022-08-23 23:49:48 -0500

    agregando sweetalert

--- js/script.js
+++ js/script.js
@@ -9,16 +9,19 @@ whitem.addEventListener("click", () => {
 
   if (document.body.classList.contains("whitemode")) {
     localStorage.setItem("white", "true");
+    Swal.fire("Estas en modo claro")
   } else {
     localStorage.setItem("white", "false");
+    Swal.fire("Estas en modo oscuro")
   }
 });
 
 if (localStorage.getItem("white") === "true") {
   document.body.classList.add("whitemode");
   whitem.classList.add("active");
+  Swal.fire("Estas en modo claro")
 } else {
   document.body.classList.remove("whitemode");
   whitem.classList.remove("active");
+  Swal.fire("Estas en modo oscuro")
 }
-
